53fe5ca4b0db9f979dbab8186aed9922de21061d
annieFromTaiwan <fewwordsmanydeeds@gmail.com>
2015-12-04 12:35:58 +0800

    Add comments to the function `my_integration`.

--- p4_integration.py
+++ p4_integration.py
@@ -5,6 +5,11 @@ def test(x):
 def anonymous(x):
 	return x**2 + 1
 
+# This version is more accurate than the orginial version and fits
+# more to the 3rd wiki picture.
+# Can't achieve this effect by only adding 1-3 lines to the original
+# funciton without modifying other lines. That's why I made this new
+# function
 def my_integrate(fun, start, end):
 	step = 0.1
 	intercept = start
